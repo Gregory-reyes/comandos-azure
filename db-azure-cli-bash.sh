dee4e83cdbf88d7241f7e1e7947bea80c360a738
Gregory Reyes <greyes3@udi.edu.co>
2023-07-26 20:55:53 -0500

    Despliegue SQL funcional con pruebas de creación de tablas y consulta ok

--- db-azure-cli-bash.sh
+++ db-azure-cli-bash.sh
@@ -9,8 +9,17 @@ az group create --name $rgname --location $location
 #crear servidor de base de datos
 az sql server create --name $servername --resource-group $rgname --location $location --admin-user demoyoutube --admin-password D!emoyoutube!!.1234!!!X
 
+#crear firewall rule para permitir conexiones desde cualquier ip
+az sql server firewall-rule create --resource-group $rgname --server $servername --name Reglaconexion --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
+
 #crear base de datos
 az sql db create --resource-group $rgname --server $servername --name demoyoutube --zone-redundant false
+
+#ruta para hacer la consulta de la base de datos
+#En grupo de recurso ingresamos a la database y en la parte de la izquierda seleccionamos query editor y colocamos la contraseña y damos ok
+#Seleccionamos la base de datos y creo la tabla y las consulta tipo management studio y le damos a ejecutar
+#en la parte izquierda dentro de la tabla damos click derecho select top 1000 rows y nos mostrara los datos de la tabla
+
 
 
 
